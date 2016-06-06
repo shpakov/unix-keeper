e98df7294927b40f450a8f5c23347670895fa016
roman <roman@shpakov.com>
2016-06-06 07:05:36 -0400

    Added RAM usage metric collection

--- go.sh
+++ go.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #Tested on Linux 3.10.0-327.18.2.el7.x86_64
 #Get CPU usage
-top -bn1 | grep "Cpu(s)" | awk '{print "cpuUsed=" 100 - $8}'
+top -bn1 | grep "Cpu(s)" | awk '{print "cpuUsed=" 100 - $8"%"}'
+#Get RAM usage
+free -m | awk 'NR==2{printf "ramUsed=%.2f%\n",$3*100/$2 }'
